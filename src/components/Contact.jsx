9b977331f7c516563803a701554701cc0d40d4bf
Alivia <43556891+17anguyen@users.noreply.github.com>
2024-03-18 11:08:30 -0700

    pushing to update my PC

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -115,6 +115,7 @@ const Contact = () => {
         variants={slideIn('right', 'tween', 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
+        
         {/* add a graphic */}
       </motion.div>
     </div>
