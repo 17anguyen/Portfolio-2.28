dcc54f8d3bff611a677fc8582f6020541ebd0612
Alivia <43556891+17anguyen@users.noreply.github.com>
2024-03-27 16:54:31 -0700

    link is weird on deploy changing

--- src/components/canvas/Blocks.jsx
+++ src/components/canvas/Blocks.jsx
@@ -2,8 +2,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
     return (
-        //    <Spline scene="https://prod.spline.design/kApfYd3OT6nHHnMC/scene.splinecode" />
-        <spline-viewer hint loading-anim-type="spinner-big-light" url="https://prod.spline.design/kApfYd3OT6nHHnMC/scene.splinecode"></spline-viewer>
+           <Spline scene="https://prod.spline.design/kApfYd3OT6nHHnMC/scene.splinecode" />
+        // <spline-viewer hint loading-anim-type="spinner-big-light" url="https://prod.spline.design/kApfYd3OT6nHHnMC/scene.splinecode"></spline-viewer>
         // <iframe src='https://my.spline.design/draganddropjengacopy-4cdef7184b56968e06d709255c453119/' frameborder='0' width='100%' height='100%'></iframe>
         
     );
