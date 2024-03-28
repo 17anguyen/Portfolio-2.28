e193d5616518ae597d34b0012be3888b9b0e57af
17anguyen <17anguyen@gmail.com>
2024-03-28 13:39:34 -0700

    fixed padding for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-gradient p-[1px] rounded-[20px] shadow-card'
+      className='w-full green-gradient p-[1px] rounded-[20px] shadow-card m-5'
     >
       <div
         options={{
